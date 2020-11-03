ac9108a12931fd16d5fd795d0ca07cc8363eb48b
Maurice Schmitz <msc@combahton.net>
2020-11-03 15:09:15 +0100

    Remove test file (test.py)

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-
-print("script: sys.argv[0] is", repr(sys.argv[0]))
-print("script: __file__ is", repr(__file__))
-print("script: cwd is", repr(os.getcwd()))
-
-import pathlib
-
-print(pathlib.Path(__file__).parent.absolute())
